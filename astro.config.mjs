f2308e67fb9e6d6b3cc4cc1bb18b8bff63da6b54
rodolfo <rodolfogarciamaeso@gmail.com>
2025-10-27 18:28:49 -0400

    subiendo proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ['gsap'],
+
+    },
+    optimizeDeps: {
+      // Optimizar GSAP para mejor rendimiento
+      include: ['gsap', 'gsap/ScrollTrigger']
+    }
+  }
+});
