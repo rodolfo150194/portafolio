d8acfc98ea7fbe59736bb4c827c413bfc131196e
Rodolfo <rodolfogarciamaeso@gmail.com>
2026-05-08 16:36:22 +0200

    feat: add Vercel adapter for static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [tailwind()],
   vite: {
     ssr: {
